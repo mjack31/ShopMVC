00be5acb7bbc1c66bc958e1762eb242514e5cff8
mjack31 <shark3113@gmail.com>
2020-04-13 16:16:06 +0200

    Add project files.

---
+++ ShopMVC/Models/Repositories/AppDbContext.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopMVC.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace ShopMVC.Models
+{
+    public class AppDbContext : IdentityDbContext<IdentityUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Pie> Pies { get; set; }
+        public DbSet<ShoppingCartItem> ShoppingCartItems { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderDetail> OrderDetails { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            //seed categories
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 1, CategoryName = "Fruit pies" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 2, CategoryName = "Cheese cakes" });
+            modelBuilder.Entity<Category>().HasData(new Category { CategoryId = 3, CategoryName = "Seasonal pies" });
+
+            //seed pies
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 1,
+                Name = "Apple Pie",
+                Price = 12.95M,
+                ShortDescription = "Our famous apple pies!",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 1,
+                ImageUrl = @"\img\applepie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = true,
+                ImageThumbnailUrl = @"\img\applepiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 2,
+                Name = "Blueberry Cheese Cake",
+                Price = 18.95M,
+                ShortDescription = "You'll love it!",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 2,
+                ImageUrl = @"\img\blueberrycheesecake.jpg",
+                InStock = true,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl =
+                    @"\img\blueberrycheesecakesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 3,
+                Name = "Cheese Cake",
+                Price = 18.95M,
+                ShortDescription = "Plain cheese cake. Plain pleasure.",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 2,
+                ImageUrl = @"\img\cheesecake.jpg",
+                InStock = true,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl = @"\img\cheesecakesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 4,
+                Name = "Cherry Pie",
+                Price = 15.95M,
+                ShortDescription = "A summer classic!",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 1,
+                ImageUrl = @"\img\cherrypie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl = @"\img\cherrypiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 5,
+                Name = "Christmas Apple Pie",
+                Price = 13.95M,
+                ShortDescription = "Happy holidays with this pie!",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 3,
+                ImageUrl = @"\img\christmasapplepie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl =
+                    @"\img\christmasapplepiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 6,
+                Name = "Cranberry Pie",
+                Price = 17.95M,
+                ShortDescription = "A Christmas favorite",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 3,
+                ImageUrl = @"\img\cranberrypie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl = @"\img\cranberrypiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 7,
+                Name = "Peach Pie",
+                Price = 15.95M,
+                ShortDescription = "Sweet as peach",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 1,
+                ImageUrl = @"\img\peachpie.jpg",
+                InStock = false,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl = @"\img\peachpiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 8,
+                Name = "Pumpkin Pie",
+                Price = 12.95M,
+                ShortDescription = "Our Halloween favorite",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 3,
+                ImageUrl = @"\img\pumpkinpie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = true,
+                ImageThumbnailUrl = @"\img\pumpkinpiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 9,
+                Name = "Rhubarb Pie",
+                Price = 15.95M,
+                ShortDescription = "My God, so sweet!",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 1,
+                ImageUrl = @"\img\rhubarbpie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = true,
+                ImageThumbnailUrl = @"\img\rhubarbpiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 10,
+                Name = "Strawberry Pie",
+                Price = 15.95M,
+                ShortDescription = "Our delicious strawberry pie!",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 1,
+                ImageUrl = @"\img\strawberrypie.jpg",
+                InStock = true,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl = @"\img\strawberrypiesmall.jpg",
+                AllergyInformation = ""
+            });
+
+            modelBuilder.Entity<Pie>().HasData(new Pie
+            {
+                PieId = 11,
+                Name = "Strawberry Cheese Cake",
+                Price = 18.95M,
+                ShortDescription = "You'll love it!",
+                LongDescription =
+                    "Icing carrot cake jelly-o cheesecake. Sweet roll marzipan marshmallow toffee brownie brownie candy tootsie roll. Chocolate cake gingerbread tootsie roll oat cake pie chocolate bar cookie dragée brownie. Lollipop cotton candy cake bear claw oat cake. Dragée candy canes dessert tart. Marzipan dragée gummies lollipop jujubes chocolate bar candy canes. Icing gingerbread chupa chups cotton candy cookie sweet icing bonbon gummies. Gummies lollipop brownie biscuit danish chocolate cake. Danish powder cookie macaroon chocolate donut tart. Carrot cake dragée croissant lemon drops liquorice lemon drops cookie lollipop toffee. Carrot cake carrot cake liquorice sugar plum topping bonbon pie muffin jujubes. Jelly pastry wafer tart caramels bear claw. Tiramisu tart pie cake danish lemon drops. Brownie cupcake dragée gummies.",
+                CategoryId = 2,
+                ImageUrl = @"\img\strawberrycheesecake.jpg",
+                InStock = false,
+                IsPieOfTheWeek = false,
+                ImageThumbnailUrl =
+                    @"\img\strawberrycheesecakesmall.jpg",
+                AllergyInformation = ""
+            });
+        }
+    }
+}
---
+++ ShopMVC/ViewModels/HomeViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ShopMVC.Models;
+
+namespace ShopMVC.ViewModels
+{
+    public class HomeViewModel
+    {
+        public IEnumerable<Pie> PiesOfTheWeek { get; set; }
+    }
+}
